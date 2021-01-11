499894cd450d53884126afda06712bb9e6495ca5
Juan Manuel Vargas <juan.vargas@unionsoluciones.com.co>
2021-01-11 14:38:04 -0500

    se modifico  la interfaz

--- src/IPersona.java
+++ src/IPersona.java
@@ -1,4 +1,11 @@
+import dto.Persona;
 
 public interface IPersona {
 
+	
+	public Persona crearPersona( Persona persona);	
+	
+	public boolean updatePersona( Integer idPersona);
+	
+	public String deletePersona( Integer idPersona);
 }
