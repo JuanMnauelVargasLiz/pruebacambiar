923d08f35a3236b92f1732e828cdc1091a0ee876
Juan Manuel Vargas <juan.vargas@unionsoluciones.com.co>
2021-01-11 15:13:39 -0500

    PROBANDO PRUEBA

--- src/dao/Constantes.java
+++ src/dao/Constantes.java
@@ -2,4 +2,9 @@ package dao;
 
 public class Constantes {
 
+	
+	private  String VARIALBE_UNO="Yes";
+	private  String VARIALBE_DOS="Not";
+	private  String VARIALBE_TRES="Ok";
+	private  String VARIALBE_CUATRO="FUNCIONA";
 }
