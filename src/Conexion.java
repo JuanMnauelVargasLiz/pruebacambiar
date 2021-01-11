3d8319ec674f046de951b299c1c21348d3de1566
Juan Manuel Vargas <juan.vargas@unionsoluciones.com.co>
2021-01-11 14:31:13 -0500

    se realiza la conexion a la bd

---
+++ src/Conexion.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+import dto.Persona;
+
+public class Conexion {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int opcion = 0;
+		Scanner scanner = new Scanner(System.in);
+		Persona producto;
+
+	
+		while (opcion!=5) {
+			System.out.println("1. Crear Producto");
+			System.out.println("2. Buscar Producto");
+			System.out.println("3. Actualizar Producto");
+			System.out.println("4. Eliminar Producto");
+			System.out.println("5. Salir");
+			System.out.println("Elija una opción:");
+
+		}}
+}
